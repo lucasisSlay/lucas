5d61d77182b84949e0bd737576fb95ed985aeb23
Lucas Wong <wong_wei_kai_lucas@s2024.ssts.edu.sg>
2024-09-09 15:58:44 +0800

    Initial Commit

---
+++ lesson 8/lesson_8App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson_8App.swift
+//  lesson 8
+//
+//  Created by Wong Wei Kai Lucas on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct lesson_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
